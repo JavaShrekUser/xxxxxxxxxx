d3ac3a06403fa025fd5b0c2db113877a80aaba98
JavaShrekUser <63369013+JavaShrekUser@users.noreply.github.com>
2020-04-17 21:48:49 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ],
+};
+
+let game = new Phaser.Game(config); 
+//reserve some keyboard variables
+let keyF, keyLEFT, keyRIGHT;
+
+game.settings = {
+    spaceshipSpeed:3,
+    gameTimer: 60000
+}
